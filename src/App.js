60cc3a14ac7cbc6d27d6de37960f77b0495fd3d2
8BitRobot <premgiridhar11@gmail.com>
2022-10-18 10:05:03 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [arrow, setArrow] = useState(0);
+  const [arrowDirection, setArrowDirection] = useState(1);
+  const [started, setStarted] = useState(false);
+  let values = ["🙂", "🙌", "🛑", "🍒", "🍏", "❌", "🎅"];
+  let els = values.map((i, count) =>
+    <div key={count}>
+      <p>{i}</p>
+      <div>{ (arrow == count) ? "⬆" : "" }</div>
+    </div>
+  );
+
+  function arrowDirectionUpdater() {
+    if (arrow == values.length) {
+      setArrowDirection(-1);
+    } else if (arrow == 0 && started) {
+      setArrowDirection(1);
+    }
+  }
+  
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setArrow(arrow + arrowDirection);
+    }, 1000);
+    setStarted(true);
+    return () => clearInterval(interval);
+  });
+
+  useEffect(() => {
+    arrowDirectionUpdater();
+  }, [arrow])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="emojis">
+          { els }
+        </div>
       </header>
     </div>
   );
